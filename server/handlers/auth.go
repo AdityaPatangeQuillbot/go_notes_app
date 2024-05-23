bfadd16b59c0b11c32a052ee78655e0c0a7fd306
Aditya Patange <aditya.patange@quillbot.com>
2024-05-24 00:26:36 +0530

    handlers/auth: add user login handler

--- server/handlers/auth.go
+++ server/handlers/auth.go
@@ -102,3 +102,79 @@ func UserSignup(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(createdUser)
 }
+
+func UserLogin(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// if r.Method != http.MethodPost {
+	// 	core.FormatErrorResponseJSON(&w, fmt.Errorf("invalid request method"), http.StatusMethodNotAllowed)
+	// 	return
+	// }
+
+	var body models.LoginRequest
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+
+	if err != nil {
+		log.Default().Println("invalid request body", err)
+		core.FormatErrorResponseJSON(&w, fmt.Errorf("invalid request body"), http.StatusBadRequest)
+		return
+	}
+
+	dbClient, err := core.GetDbClient()
+
+	if err != nil {
+		core.FormatErrorResponseJSON(&w, fmt.Errorf("database connection error"), http.StatusInternalServerError)
+		return
+	}
+
+	ctx := context.Background()
+
+	user, err := dbClient.User.FindFirst(db.User.Email.Equals(body.Username)).Exec(ctx)
+
+	if err != nil {
+		core.FormatErrorResponseJSON(&w, fmt.Errorf("system error"), http.StatusInternalServerError)
+		return
+	}
+
+	if user == nil {
+		core.FormatErrorResponseJSON(&w, fmt.Errorf("user not found"), http.StatusNotFound)
+		return
+	}
+
+	hashedPassword, err := dbClient.Passwords.FindFirst(db.Passwords.UserID.Equals(user.ID)).Exec(ctx)
+
+	if err != nil {
+		core.FormatErrorResponseJSON(&w, fmt.Errorf("system error"), http.StatusInternalServerError)
+		return
+	}
+
+	if err != nil {
+		core.FormatErrorResponseJSON(&w, fmt.Errorf("system error"), http.StatusInternalServerError)
+		return
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(hashedPassword.PasswordHash), []byte(body.Password)) != nil {
+		core.FormatErrorResponseJSON(&w, fmt.Errorf("invalid password"), http.StatusBadRequest)
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userId":   user.ID,
+		"username": user.Email,
+		"nbf":      time.Now().Unix(),
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte("secret"))
+
+	if err != nil {
+		core.FormatErrorResponseJSON(&w, fmt.Errorf("system error"), http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"token": tokenString,
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
