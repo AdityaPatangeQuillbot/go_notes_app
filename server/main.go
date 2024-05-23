c93643d0ae7eea82a1e8111d851504b2a4a3bc7d
Aditya Patange <aditya.patange@quillbot.com>
2024-05-23 20:49:22 +0530

    main.go: add signup endpoint

--- server/main.go
+++ server/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"crypto/subtle"
+	"encoding/json"
 	"fmt"
+	"main/core"
+	"main/db"
+	"main/models"
 	"net/http"
 
-	"github.com/AdityaPatangeQuillBot/go_notes_app/server/core"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +27,58 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func UserSignup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	ctx := context.Background()
+	rawBody := r.Body
+
+	if rawBody == nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	}
+
+	var data models.SignupRequest
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	}
+
+	// timing safe comparison
+	if subtle.ConstantTimeCompare([]byte(data.Password), []byte(data.ConfirmPassword)) == 0 {
+		http.Error(w, "Passwords do not match", http.StatusBadRequest)
+	}
+
+	dbClient, err := core.GetDbClient()
+
+	if err != nil {
+		http.Error(w, "Database connection error", http.StatusInternalServerError)
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(data.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		http.Error(w, "System error", http.StatusInternalServerError)
+	}
+
+	// create user
+	createdUser, err := dbClient.User.CreateOne(db.User.Email.Set(data.Username)).Exec(ctx)
+
+	if err != nil || createdUser == nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+	}
+
+	storedPasswordHash, err := dbClient.Passwords.CreateOne(db.Passwords.User.Link(db.User.ID.Equals(createdUser.ID)), db.Passwords.PasswordHash.Set(string(hashedPassword))).Exec(ctx)
+
+	if err != nil || storedPasswordHash == nil {
+		http.Error(w, "Failed to store password", http.StatusInternalServerError)
+	}
+
+	json.NewEncoder(w).Encode(createdUser)
+}
+
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/signup", UserSignup)
 	http.ListenAndServe(fmt.Sprintf(":%s", core.DEFAULT_PORT), nil)
 }
